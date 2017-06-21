2baa232add0e6227dbb793e133b83e2d00b1e9cf
Tyler Holloway <htoyll@fb.com>
2017-06-21 13:04:42 -0700

    added tableview

---
+++ Tumblr/PhotosTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  PhotosTableViewCell.swift
+//  Tumblr
+//
+//  Created by Tyler Holloway on 6/21/17.
+//  Copyright © 2017 Tyler Holloway. All rights reserved.
+//
+
+import UIKit
+
+class PhotosTableViewCell: UITableViewCell {
+
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
